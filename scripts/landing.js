6e9f031b9af0d3ed895f2d58c1a98205fa8b94cd
Shatadipa Chakraborty <shatadipa1986@gmail.com>
2016-08-28 16:28:07 -0700

    events assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -3,17 +3,27 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
 
-    var revealPoint = function(i) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(1)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    // var revealPoint = function(i) {
+    //     points[i].style.opacity = 1;
+    //     points[i].style.transform = "scaleX(1) translateY(1)";
+    //     points[i].style.msTransform = "scaleX(1) translateY(0)";
+    //     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+    // };
+
+    // for (var i = 0; i < points.length; i++) {
+    //     revealPoint(i);
+    // }
+
+    var revealPoint = function(point) {
+        point.style.opacity = 1;
+        point.style.transform = "scaleX(1) translateY(1)";
+        point.style.msTransform = "scaleX(1) translateY(0)";
+        point.style.WebkitTransform = "scaleX(1) translateY(0)";
     };
 
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    }
+    forEach(points, revealPoint);
 };
+
 
 window.onload = function() {
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
@@ -24,8 +34,6 @@ window.onload = function() {
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
     window.addEventListener('scroll', function(event) {
-        console.log('scroll top ' + document.body.scrollTop)
-        console.log('scroll distance ' + scrollDistance)
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
             animatePoints(pointsArray);
         }
---
+++ scripts/utilities.js
@@ -0,0 +1,5 @@
+var forEach = function(arr, callback) {
+    for (var i = 0; i < arr.length; i++) {
+        callback(arr[i]);
+    }
+};
